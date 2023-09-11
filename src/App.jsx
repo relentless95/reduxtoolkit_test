09508e5a37430ac91a3c8de4e208e397729e52cc
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-09-11 18:51:48 +0200

    feat: added the calculate totals functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
+import { calculateTotals } from "./features/cart/cartSlice";
+import cartItems from "./cartItems";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
 
   return (
     <>
